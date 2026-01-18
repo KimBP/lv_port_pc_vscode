37245d5ca51f5b5695774eb23344cda374f8049d
Kim Bøndergaard <kim@fam-boendergaard.dk>
2026-01-18 20:25:16 +0100

    Rebase preparation
    main/src/ -> src/

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
-
 /**
- * @file main
+ * @file main.cpp
  *
  */
 
 /*********************
  *      INCLUDES
  *********************/
-#define _DEFAULT_SOURCE /* needed for usleep() */
+
+#ifndef _DEFAULT_SOURCE
+  #define _DEFAULT_SOURCE /* needed for usleep() */
+#endif
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
+#ifdef _MSC_VER
+  #include <Windows.h>
+#else
+  #include <unistd.h>
+  #include <pthread.h>
+#endif
 #include "lvgl/lvgl.h"
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
-#include "glob.h"
+#include <SDL.h>
+
+#include "hal/hal.h"
 #include "lvglpp/examples/examples.h"
 
 /*********************
@@ -29,13 +37,12 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static lv_display_t * hal_init(int32_t w, int32_t h);
 
 /**********************
  *  STATIC VARIABLES
  **********************/
 
-/********************** 
+/**********************
  *      MACROS
  **********************/
 
@@ -43,27 +50,7 @@ static lv_display_t * hal_init(int32_t w, int32_t h);
  *   GLOBAL FUNCTIONS
  **********************/
 
-extern void freertos_main(void);
-
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *      VARIABLES
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
+# LV_USE_OS == LV_OS_NONE
 
 int main(int argc, char **argv)
 {
@@ -74,10 +61,14 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  hal_init(320, 480);
+  sdl_hal_init(320, 480);
 
-  #if LV_USE_OS == LV_OS_NONE
- 
+  /* Run the default demo */
+  /* To try a different demo or example, replace this with one of: */
+  /* - lv_demo_stress(); */
+  /* or the ones below */
+  lv_demo_widgets();
+
   /* C++ implementations */
 //  lvgl::examples::switch_1();
 //  lvgl::examples::calendar_1();
@@ -117,59 +108,28 @@ int main(int argc, char **argv)
 //  lv_example_label_1();
 //  lv_example_label_4();
 //  lv_example_canvas_1();
-  
-lv_demo_widgets();
 
   while(1) {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
-    lv_timer_handler();
-    usleep(5 * 1000);
+    uint32_t sleep_time_ms = lv_timer_handler();
+    if (sleep_time_ms == LV_NO_TIMER_READY) {
+      sleep_time_ms = LV_DEF_REFR_PERIOD;
+    }
+#ifdef _MSC_VER
+    Sleep(sleep_time_ms);
+#else
+    usleep(sleep_time_ms * 1000);
+#endif
+
+    return 0
   }
-
-  #elif LV_USE_OS == LV_OS_FREERTOS
-
-  /* Run FreeRTOS and create lvgl task */
-  freertos_main();  
-
-  #endif
 
   return 0;
 }
 
+#endif
+
 /**********************
  *   STATIC FUNCTIONS
  **********************/
-
-/**
- * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
- * library
- */
-static lv_display_t * hal_init(int32_t w, int32_t h)
-{
-
-  lv_group_set_default(lv_group_create());
-
-  lv_display_t * disp = lv_sdl_window_create(w, h);
-
-  lv_indev_t * mouse = lv_sdl_mouse_create();
-  lv_indev_set_group(mouse, lv_group_get_default());
-  lv_indev_set_display(mouse, disp);
-  lv_display_set_default(disp);
-
-  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-  lv_obj_t * cursor_obj;
-  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
-
-  lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
-  lv_indev_set_display(mousewheel, disp);
-  lv_indev_set_group(mousewheel, lv_group_get_default());
-
-  lv_indev_t * kb = lv_sdl_keyboard_create();
-  lv_indev_set_display(kb, disp);
-  lv_indev_set_group(kb, lv_group_get_default());
-
-  return disp;
-}
